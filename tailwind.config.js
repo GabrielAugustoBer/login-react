63e4f9ae4c21edd5c505d4bfacf9be8f060107b7
Gabriel Augusto <92814110+GabrielAugustoBer@users.noreply.github.com>
2024-05-09 10:26:57 -0300

    feat: add theme and component

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ['var(--font-family-poppins)']
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
